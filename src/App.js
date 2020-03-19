9e663c1f745961a4775d17b5e045ce9b6ddf9e1b
hudson21 <camihudsondiaz@ittepic.edu.mx>
2020-03-19 11:36:47 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Componetns
+import Homepage from "./homepage.component";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
