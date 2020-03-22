f565a41d2e498d53e1bee0733d8bdc48a9d6e897
hudson21 <camihudsondiaz@ittepic.edu.mx>
2020-03-22 11:37:23 -0500

    Finished Homepage View

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+// Components
+import Directory from '../../components/directory/directory.component';
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+)
+
+export default Homepage;
