93690f0d422cbdbfda932183bcd4d6b03a7d65d6
hudson21 <camihudsondiaz@ittepic.edu.mx>
2020-03-26 14:20:04 -0500

    Starting Module 7

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 // Componetns
-import Homepage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop/hats" component={HatsPage}/>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage}/>
+        <Route exact path="/signin" component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+// Helpers
+import  SHOP_DATA  from './shop.data';
+
+// Components
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+  render() {  
+    const { collections } = this.state;
+    return(
+      <div className='shop-page'>
+        {
+          collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview 
+              key={id}
+              {...otherCollectionProps}
+            />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
