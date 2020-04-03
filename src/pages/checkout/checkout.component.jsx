20ab7e81415dd0788461ccfc84cb429e62bdb9d1
hudson21 <camihudsondiaz@ittepic.edu.mx>
2020-04-03 17:31:31 -0500

    Module 14 Deploying to Production

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,6 +10,7 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 
 // Components
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className='checkout-page'>
@@ -38,6 +39,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+      * Please use the following test credit card for payments*
+      <br/>
+      4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
