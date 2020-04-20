b3c7e7c36e7e86d77127c173000012eb2522fffb
hudson21 <camihudsondiaz@ittepic.edu.mx>
2020-04-20 16:38:17 -0500

    Added Stripe Payments Handler

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_zdkwrH1kGILw1N4OT88JxhS7';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+    .then(response => {
+      alert('Payment was successful')
+    })
+    .catch(error => {
+      console.log(`Payment Error: ${JSON.parse(error)}`);
+      alert('There was an issue with your payment. Please make sure you use the provided credit card');
+    });
   }
 
   return(
